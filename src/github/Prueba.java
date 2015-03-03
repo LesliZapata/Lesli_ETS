aca136c8becfcd7314572969df13639d6f771cc4
Sam <Sam@Aula-B4-PC-2>
2015-03-03 11:20:05 +0000

    Version 3

--- src/github/Prueba.java
+++ src/github/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Segunda prueba para ETS de Lesli");
+		System.out.print("Tercera prueba para ETS de Lesli");
 	}
 
 }
