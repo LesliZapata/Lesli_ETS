6ec4a64fb07651368f90350898a26f653039467b
Sam <Sam@Aula-B4-PC-2>
2015-03-24 12:52:17 +0000

    Repositorio Vr1

---
+++ src/github/ejemplo.java
@@ -0,0 +1,13 @@
+package github;
+
+public class ejemplo {
+
+	public static void main(String[] args) {
+
+		fantasticLesli();
+	}
+	
+	public static void fantasticLesli(){
+			System.out.print("Soy Lesli, soy fantastica");
+		}
+}
